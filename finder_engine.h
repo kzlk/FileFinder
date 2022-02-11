ee00838e9ebc49a6ff634a8be49e6c5210078a8a
Kozliuk Dmytro <64733759+DmitriyProg@users.noreply.github.com>
2022-02-11 21:34:08 +0200

    Update finder_engine.h

--- finder_engine.h
+++ finder_engine.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "stdafx.h"
 #define MAX_THREAD 8
 namespace fs = std::filesystem;
@@ -7,23 +7,22 @@ class thread_pool
 {
 public:
 
-    explicit thread_pool(int threads);
+	explicit thread_pool(int threads);
     thread_pool();
     ~thread_pool();
 
-    void execute(std::function <void(void)> func);
+    void execute(std::function <void()> func);
 
 protected:
 
     void threadEntry(int i);
-    std::mutex lock_;
+    std::mutex lock_; 
     std::condition_variable condVar_;
     bool shutdown_ = false;
-    std::queue <std::function <void(void)>> jobs_;
+
+    std::queue <std::function <void()>> jobs_;
+
     std::vector <std::thread> threads_;
-
-    bool stopped_ = false;
-
 
 
 };
@@ -31,21 +30,26 @@ protected:
 class finder_engine: public thread_pool
 {
 public:
-
-    std::atomic<bool> exit_thread_flag{ false };
-
     finder_engine();
+    ~finder_engine();
 
-   std::vector<std::string>
+     std::vector<std::string>
         getListSubDir(std::string const& dir, std::string const& app_name);
 
-    std::vector<std::string> getListOfDrives();
+    static inline std::vector<std::string> getListOfDrives();
 
-     int get_num_thread();
+    inline int get_num_thread();
 
     std::string get_path_by_name(std::string const& app_name);
 
+    
+
+private:
+    std::string path = "NoFound";
+    int available_thread = 0;
     void Finder(std::string const& dir, std::string const& name, std::string& result);
+    std::atomic<bool> exit_thread_flag{ false };
    
+    
 };
 
